2a8f1e7147f3e13f508914d61547d7670b0f35df
Geison Biazus <geisonbiazus@gmail.com>
2020-06-19 09:21:12 +0200

    Score value matches

--- MasterMind/src/codeMaker.test.ts
+++ MasterMind/src/codeMaker.test.ts
@@ -1,12 +1,25 @@
 import { score, Match } from "./codeMaker";
 
 describe("Code Maker", () => {
-  it("Score guess same position macthes", () => {
-    expect(score([0, 0, 0, 0], [1, 1, 1, 1])).toEqual({ pos: 0 });
-    expect(score([0, 0, 0, 0], [0, 1, 1, 1])).toEqual({ pos: 1 });
-    expect(score([0, 0, 0, 0], [0, 1, 1, 0])).toEqual({ pos: 2 });
-    expect(score([1, 1, 1, 1], [0, 1, 1, 1])).toEqual({ pos: 3 });
-    expect(score([0, 0, 0, 0], [0, 0, 0, 1])).toEqual({ pos: 3 });
-    expect(score([1, 2, 3, 4], [1, 2, 3, 4])).toEqual({ pos: 4 });
+  it("Scores same position matches", () => {
+    expect(score([0, 0, 0, 0], [1, 1, 1, 1]).pos).toEqual(0);
+    expect(score([0, 0, 0, 0], [0, 1, 1, 1]).pos).toEqual(1);
+    expect(score([0, 0, 0, 0], [0, 1, 1, 0]).pos).toEqual(2);
+    expect(score([1, 1, 1, 1], [0, 1, 1, 1]).pos).toEqual(3);
+    expect(score([0, 0, 0, 0], [0, 0, 0, 1]).pos).toEqual(3);
+    expect(score([1, 2, 3, 4], [1, 2, 3, 4]).pos).toEqual(4);
+  });
+
+  it("Scores same value macthes", () => {
+    expect(score([1, 2, 3, 4], [2, 0, 0, 0]).val).toEqual(1);
+    expect(score([1, 2, 3, 4], [2, 3, 0, 0]).val).toEqual(2);
+    expect(score([1, 2, 3, 4], [2, 3, 4, 0]).val).toEqual(3);
+    expect(score([1, 2, 3, 4], [2, 3, 4, 1]).val).toEqual(4);
+  });
+
+  it("Scores positions and values together", () => {
+    expect(score([1, 2, 3, 4], [2, 0, 0, 0])).toEqual({ pos: 0, val: 1 });
+    expect(score([1, 2, 3, 4], [2, 3, 0, 4])).toEqual({ pos: 1, val: 2 });
+    expect(score([1, 2, 3, 4], [1, 2, 4, 3])).toEqual({ pos: 2, val: 2 });
   });
 });
--- MasterMind/src/codeMaker.ts
+++ MasterMind/src/codeMaker.ts
@@ -1,15 +1,23 @@
 import { Code } from "./codeBreaker";
 
 export interface Match {
-  pos: number;
+  pos: number; // Digits with the same position and value
+  val: number; // Digits with the correct value but different position
 }
 
 export const score = (code: Code, guess: Code): Match => {
-  return { pos: positionMatches(code, guess) };
+  const pos = positionMatches(code, guess);
+  const val = valueMatches(code, guess);
+  return { pos, val: val - pos };
 };
 
 const positionMatches = (code: Code, guess: Code): number => {
   return code
     .map((value, index) => value == guess[index])
     .filter((value) => value).length;
+};
+
+const valueMatches = (code: Code, guess: Code): number => {
+  return guess.map((value) => code.includes(value)).filter((value) => value)
+    .length;
 };
