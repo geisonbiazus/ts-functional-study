1dce8d3d0ee1aef62768030d192a921659a782a1
Geison Biazus <geisonbiazus@gmail.com>
2020-06-23 09:17:04 +0200

    add numberToCode

--- MasterMind/src/codeBreaker.test.ts
+++ MasterMind/src/codeBreaker.test.ts
@@ -1,4 +1,4 @@
-import { breakCode, guessToNumber } from "./codeBreaker";
+import { breakCode, codeToNumber, numberToCode } from "./codeBreaker";
 
 describe("Code Breaker", () => {
   it("initial guess", () => {
@@ -6,18 +6,36 @@ describe("Code Breaker", () => {
   });
 });
 
-describe("guessToNumber", () => {
+describe("codeToNumber", () => {
   it("converts the guess into an integer number", () => {
-    expect(guessToNumber([0, 0, 0, 0])).toEqual(0);
-    expect(guessToNumber([0, 0, 0, 1])).toEqual(1);
-    expect(guessToNumber([0, 0, 0, 5])).toEqual(5);
-    expect(guessToNumber([0, 0, 1, 0])).toEqual(6);
-    expect(guessToNumber([0, 0, 1, 1])).toEqual(7);
-    expect(guessToNumber([0, 0, 1, 5])).toEqual(11);
-    expect(guessToNumber([0, 0, 2, 0])).toEqual(12);
-    expect(guessToNumber([0, 1, 0, 0])).toEqual(36);
-    expect(guessToNumber([0, 1, 0, 1])).toEqual(37);
-    expect(guessToNumber([0, 1, 1, 1])).toEqual(43);
-    expect(guessToNumber([1, 1, 1, 1])).toEqual(259);
+    expect(codeToNumber([0, 0, 0, 0])).toEqual(0);
+    expect(codeToNumber([0, 0, 0, 1])).toEqual(1);
+    expect(codeToNumber([0, 0, 0, 5])).toEqual(5);
+    expect(codeToNumber([0, 0, 1, 0])).toEqual(6);
+    expect(codeToNumber([0, 0, 1, 1])).toEqual(7);
+    expect(codeToNumber([0, 0, 1, 5])).toEqual(11);
+    expect(codeToNumber([0, 0, 2, 0])).toEqual(12);
+    expect(codeToNumber([0, 1, 0, 0])).toEqual(36);
+    expect(codeToNumber([0, 1, 0, 1])).toEqual(37);
+    expect(codeToNumber([0, 1, 1, 1])).toEqual(43);
+    expect(codeToNumber([1, 1, 1, 1])).toEqual(259);
+    expect(codeToNumber([5, 5, 5, 5])).toEqual(6 * 6 * 6 * 6 - 1);
+  });
+});
+
+describe("numberToCode", () => {
+  it("converts an integer into a guess", () => {
+    expect(numberToCode(0)).toEqual([0, 0, 0, 0]);
+    expect(numberToCode(1)).toEqual([0, 0, 0, 1]);
+    expect(numberToCode(5)).toEqual([0, 0, 0, 5]);
+    expect(numberToCode(6)).toEqual([0, 0, 1, 0]);
+    expect(numberToCode(7)).toEqual([0, 0, 1, 1]);
+    expect(numberToCode(11)).toEqual([0, 0, 1, 5]);
+    expect(numberToCode(12)).toEqual([0, 0, 2, 0]);
+    expect(numberToCode(36)).toEqual([0, 1, 0, 0]);
+    expect(numberToCode(37)).toEqual([0, 1, 0, 1]);
+    expect(numberToCode(43)).toEqual([0, 1, 1, 1]);
+    expect(numberToCode(259)).toEqual([1, 1, 1, 1]);
+    expect(numberToCode(6 * 6 * 6 * 6 - 1)).toEqual([5, 5, 5, 5]);
   });
 });
--- MasterMind/src/codeBreaker.ts
+++ MasterMind/src/codeBreaker.ts
@@ -4,7 +4,20 @@ export const breakCode = (pastGuesses: Code[]): Code => {
   return [0, 0, 0, 0];
 };
 
-export const guessToNumber = (guess: Code): number => {
-  // return guess[0] * 6 * 6 * 6 + guess[1] * 6 * 6 + guess[2] * 6 + guess[3];
-  return guess.reduce((acc, value) => acc * 6 + value);
+export const codeToNumber = (code: Code): number => {
+  // return code[0] * 6 * 6 * 6 + code[1] * 6 * 6 + code[2] * 6 + code[3];
+  return code.reduce((acc, value) => acc * 6 + value);
+};
+
+export const numberToCode = (number: number): Code => {
+  return [
+    div(number, 6 * 6 * 6),
+    div(number, 6 * 6) % 6,
+    div(number, 6) % 6,
+    number % 6,
+  ];
+};
+
+const div = (dividend: number, divisor: number): number => {
+  return Math.floor(dividend / divisor);
 };
