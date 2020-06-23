939d590545825fd5195666a7b34e5871a5e240a7
Geison Biazus <geisonbiazus@gmail.com>
2020-06-23 08:41:14 +0200

    Add guessToNumber

--- MasterMind/src/codeBreaker.test.ts
+++ MasterMind/src/codeBreaker.test.ts
@@ -1,7 +1,23 @@
-import { breakCode } from "./codeBreaker";
+import { breakCode, guessToNumber } from "./codeBreaker";
 
 describe("Code Breaker", () => {
   it("initial guess", () => {
     expect(breakCode([])).toEqual([0, 0, 0, 0]);
+  });
+});
+
+describe("guessToNumber", () => {
+  it("converts the guess into an integer number", () => {
+    expect(guessToNumber([0, 0, 0, 0])).toEqual(0);
+    expect(guessToNumber([0, 0, 0, 1])).toEqual(1);
+    expect(guessToNumber([0, 0, 0, 5])).toEqual(5);
+    expect(guessToNumber([0, 0, 1, 0])).toEqual(6);
+    expect(guessToNumber([0, 0, 1, 1])).toEqual(7);
+    expect(guessToNumber([0, 0, 1, 5])).toEqual(11);
+    expect(guessToNumber([0, 0, 2, 0])).toEqual(12);
+    expect(guessToNumber([0, 1, 0, 0])).toEqual(36);
+    expect(guessToNumber([0, 1, 0, 1])).toEqual(37);
+    expect(guessToNumber([0, 1, 1, 1])).toEqual(43);
+    expect(guessToNumber([1, 1, 1, 1])).toEqual(259);
   });
 });
--- MasterMind/src/codeBreaker.ts
+++ MasterMind/src/codeBreaker.ts
@@ -3,3 +3,8 @@ export type Code = [number, number, number, number];
 export const breakCode = (pastGuesses: Code[]): Code => {
   return [0, 0, 0, 0];
 };
+
+export const guessToNumber = (guess: Code): number => {
+  // return guess[0] * 6 * 6 * 6 + guess[1] * 6 * 6 + guess[2] * 6 + guess[3];
+  return guess.reduce((acc, value) => acc * 6 + value);
+};
