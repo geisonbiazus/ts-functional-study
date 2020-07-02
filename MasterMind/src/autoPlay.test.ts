93e44d560bcc55a2a454f791741e4b93a87f4b27
Geison Biazus <geisonbiazus@gmail.com>
2020-07-02 08:57:39 +0200

    Add a non working version of the strategy analysis

--- MasterMind/src/autoPlay.test.ts
+++ MasterMind/src/autoPlay.test.ts
@@ -1,21 +1,73 @@
-import { autoPlay, randomCode } from './autoPlay';
-import { Code } from './codeBreaker';
+import { autoPlay, randomCode, analyzeStrategies } from './autoPlay';
+import { Code, breakCodeSeq } from './codeBreaker';
 
 describe('autoPlay', () => {
-  it('breaks teh code in 1 try if initial code is correct', () => {
-    expect(autoPlay([0, 0, 0, 0])).toEqual(1);
+  it('breaks the code with 1 try if initial code is correct', () => {
+    expect(autoPlay(breakCodeSeq, [0, 0, 0, 0])).toEqual(1);
   });
 
-  it('breaks the code [0, 0, 0, 1] in 2 tries', () => {
-    expect(autoPlay([0, 0, 0, 1])).toEqual(2);
+  it('breaks the code [0, 0, 0, 1] with 2 tries', () => {
+    expect(autoPlay(breakCodeSeq, [0, 0, 0, 1])).toEqual(2);
   });
 
-  it('breaks the code [0, 0, 1, 0] in 3 tries', () => {
-    expect(autoPlay([0, 0, 1, 0])).toEqual(3);
+  it('breaks the code [0, 0, 1, 0] with 3 tries', () => {
+    expect(autoPlay(breakCodeSeq, [0, 0, 1, 0])).toEqual(3);
   });
 
-  it('breaks a random code and returns the tris when the code is not provided', () => {
-    expect(autoPlay()).toBeGreaterThan(0);
+  it('breaks a random code and returns the tries when the code is not provided', () => {
+    expect(autoPlay(breakCodeSeq)).toBeGreaterThan(0);
+  });
+});
+
+describe.skip('analyze_strategies', () => {
+  it('plays the game many times and returns statistical results for each strategy', () => {
+    let codes: Code[] = [
+      [0, 5, 0, 3],
+      [0, 2, 2, 1],
+      [3, 5, 1, 0],
+      [1, 1, 1, 1],
+      [0, 5, 0, 3],
+      [0, 2, 2, 1],
+      [3, 5, 1, 0],
+      [1, 1, 1, 1],
+      [0, 5, 0, 3],
+      [0, 2, 2, 1],
+      [3, 5, 1, 0],
+      [1, 1, 1, 1],
+    ];
+
+    const fakeRandomCode = (): Code => {
+      const [next, ...rest] = codes;
+      codes = rest;
+      return next;
+    };
+
+    expect(analyzeStrategies(4, fakeRandomCode)).toEqual({
+      _3x2: {
+        hist: [2, 2],
+        max: 6,
+        mean: 5.5,
+        median: 6,
+        min: 5,
+        sigma: 0.5773502691896257,
+      },
+      double_rainbow: {
+        hist: [1, 2, 1],
+        max: 6,
+        mean: 5.0,
+        median: 5,
+        min: 4,
+        sigma: 0.816496580927726,
+      },
+      seq: {
+        hist: [1, 1, 2],
+        max: 6,
+        mean: 4.75,
+        median: 6,
+        min: 2,
+        sigma: 1.8929694486000912,
+      },
+    });
   });
 });
 
--- MasterMind/src/autoPlay.ts
+++ MasterMind/src/autoPlay.ts
@@ -1,21 +1,56 @@
-import { Code, breakCodeSeq, ScoredGuess, numberToCode } from './codeBreaker';
+import { Code, breakCodeSeq, ScoredGuess, numberToCode, breakCode3x2, breakCodeDoubleRainbow } from './codeBreaker';
 import { calculateScore, Score } from './codeMaker';
-
-export const autoPlay = (code: Code = randomCode()): number => {
-  return tryNextGuess(1, null, code, []);
-};
 
 export const randomCode = (): Code => {
   return numberToCode(Math.floor(Math.random() * (6 * 6 * 6 * 6 - 1)));
 };
 
-const tryNextGuess = (tries: number, lastGuess: Code | null, code: Code, pastGuesses: ScoredGuess[]): number => {
-  const guess = breakCodeSeq(lastGuess, pastGuesses);
+export const analyzeStrategies = (timesToRun: number, randomCodeFn = randomCode) => {
+  return {
+    seq: analyzeStrategy(breakCodeSeq, timesToRun, randomCodeFn),
+    _3x2: analyzeStrategy(breakCode3x2, timesToRun, randomCodeFn),
+    double_rainbow: analyzeStrategy(breakCodeDoubleRainbow, timesToRun, randomCodeFn),
+  };
+};
+
+type StrategyFn = (lastGuess: Code | null, pastGuesses: ScoredGuess[]) => Code;
+
+const analyzeStrategy = (strategy: StrategyFn, timesToRun: number, randomCodeFn: () => Code) => {
+  const tries: number[] = [];
+
+  for (let i = 0; i < timesToRun; i++) {
+    tries.push(autoPlay(strategy, randomCodeFn()));
+  }
+
+  tries.sort();
+
+  return {
+    mean: mean(tries),
+    sigma: sigma(tries),
+    min: tries[0],
+    max: tries[tries.length - 1],
+    median: median(tries),
+    hist: hist(tries),
+  };
+};
+
+export const autoPlay = (strategy: StrategyFn, code: Code = randomCode()): number => {
+  return tryNextGuess(strategy, 1, null, code, []);
+};
+
+const tryNextGuess = (
+  strategy: StrategyFn,
+  tries: number,
+  lastGuess: Code | null,
+  code: Code,
+  pastGuesses: ScoredGuess[],
+): number => {
+  const guess = strategy(lastGuess, pastGuesses);
   const score = calculateScore(code, guess);
 
   if (isFullScore(score)) return tries;
 
-  return tryNextGuess(++tries, guess, code, appendScoredGuess(pastGuesses, guess, score));
+  return tryNextGuess(strategy, ++tries, guess, code, appendScoredGuess(pastGuesses, guess, score));
 };
 
 const isFullScore = (score: Score) => {
@@ -24,4 +59,33 @@ const isFullScore = (score: Score) => {
 
 const appendScoredGuess = (scoredGuesses: ScoredGuess[], guess: Code, score: Score) => {
   return [...scoredGuesses, { guess, score }];
+};
+
+const sum = (x: number[]): number => {
+  return x.reduce((acc, v) => acc + v, 0);
+};
+
+const mean = (x: number[]): number => {
+  return sum(x) / x.length;
+};
+
+const sigma = (x: number[]): number => {
+  const mn = mean(x);
+  const v = sum(x.map(v => Math.pow(v - mn, 2)));
+  return Math.sqrt(v / (x.length - 1));
+};
+
+const median = (x: number[]): number => {
+  return x[Math.floor(x.length / 2)];
+};
+
+const hist = (x: number[]): number[] => {
+  let previous = -1;
+  const hist: number[] = [];
+  x.forEach(v => {
+    if (v === previous) hist[hist.length - 1]++;
+    else hist.push(1);
+    previous = v;
+  });
+  return hist;
 };
